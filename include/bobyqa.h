081bd719626a14e97c1af67be2fdcd5ce1e494db
elsid <elsid.mail@gmail.com>
2015-08-23 12:44:08 +0300

    Add code

---
+++ include/bobyqa.h
@@ -0,0 +1,51 @@
+#ifndef BOBYQA_H
+#define BOBYQA_H
+
+typedef double (*Function)(long n, const double *x);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* This subroutine seeks the least value of a function of many variables,
+ * by applying a trust region method that forms quadratic models by
+ * interpolation. There is usually some freedom in the interpolation
+ * conditions, which is taken up by minimizing the Frobenius norm of
+ * the change to the second derivative of the model, beginning with the
+ * zero matrix. The values of the variables are constrained by upper and
+ * lower bounds. The arguments of the subroutine are as follows. */
+
+/* N must be set to the number of variables and must be at least two.
+ * NPT is the number of interpolation conditions. Its value must be in
+ *   the interval [N+2,(N+1)(N+2)/2]. Choices that exceed 2*N+1 are not
+ *   recommended.
+ * Initial values of the variables must be set in X(1),X(2),...,X(N). They
+ *   will be changed to the values that give the least calculated F.
+ * For I=1,2,...,N, XL(I) and XU(I) must provide the lower and upper
+ *   bounds, respectively, on X(I). The construction of quadratic models
+ *   requires XL(I) to be strictly less than XU(I) for each I. Further,
+ *   the contribution to a model from changes to the I-th variable is
+ *   damaged severely by rounding errors if XU(I)-XL(I) is too small.
+ * RHOBEG and RHOEND must be set to the initial and final values of a trust
+ *   region radius, so both must be positive with RHOEND no greater than
+ *   RHOBEG. Typically, RHOBEG should be about one tenth of the greatest
+ *   expected change to a variable, while RHOEND should indicate the
+ *   accuracy that is required in the final values of the variables. An
+ *   error return occurs if any of the differences XU(I)-XL(I), I=1,...,N,
+ *   is less than 2*RHOBEG.
+ * MAXFUN must be set to an upper bound on the number of calls of CALFUN.
+ * The array W will be used for working space. Its length must be at least
+ *   (NPT+5)*(NPT+N)+3*N*(N+5)/2. */
+
+double bobyqa(const Function function, long n, long npt, double *x,
+    const double *xl, const double *xu, double rhobeg, double rhoend,
+    long maxfun, double *w);
+
+#ifdef __cplusplus
+}
+#endif
+
+#define BOBYQA_WORKING_SPACE_SIZE(n, npt) \
+    (npt + 5)*(npt + n) + 3*n*(n + 5)/2
+
+#endif // BOBYQA_H
---
+++ src/example.c
@@ -0,0 +1,62 @@
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <bobyqa.h>
+
+static void print_array(const size_t size, const double *values) {
+    printf("[%lu]", size);
+    for (size_t i = 0; i < size; ++i) {
+        printf(" %lf", values[i]);
+    }
+}
+
+static size_t function_calls_count = 0;
+
+static double function(long n, const double *x) {
+    assert(n == 2);
+    ++function_calls_count;
+    return -4*x[0]*x[1] + 5*x[0]*x[0] + 8*x[1]*x[1] + 16*sqrt(5.0)*x[0] + 8*sqrt(5.0)*x[1] - 44.0;
+}
+
+int main(int argc, char **argv) {
+    const int variables_count = 2;
+//    const int number_of_interpolation_conditions = variables_count + 2;
+    const int number_of_interpolation_conditions = (variables_count + 1)*(variables_count + 2)/2;
+    double variables_values[] = {0.0, -sqrt(5.0)};
+    const double lower_bound[] = {-4.0, -3.0};
+    const double upper_bound[] = {5.0, 5.0};
+    const double initial_trust_region_radius = 1e-3;
+    const double final_trust_region_radius = 1e3;
+    const int max_function_calls_count = 100;
+    const size_t working_space_size = BOBYQA_WORKING_SPACE_SIZE(variables_count,
+                                                                number_of_interpolation_conditions);
+    double working_space[working_space_size];
+
+    printf("initial: ");
+    print_array(variables_count, variables_values);
+    printf("\n");
+
+    const double result = bobyqa(
+            function,
+            variables_count,
+            number_of_interpolation_conditions,
+            variables_values,
+            lower_bound,
+            upper_bound,
+            initial_trust_region_radius,
+            final_trust_region_radius,
+            max_function_calls_count,
+            working_space);
+
+    printf("final: ");
+    print_array(variables_count, variables_values);
+    printf("\n");
+
+    printf("function_calls_count: %ld\n", function_calls_count);
+
+    printf("result: %lf\n", result);
+
+    return 0;
+}
